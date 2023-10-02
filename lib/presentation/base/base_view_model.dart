8dcbf5550cf8ce1ae9d357c9089db4f73ead9fd4
salah saleh <salahsaleh1015@gmail.com>
2023-10-02 15:24:13 +0300

    add base view model

---
+++ lib/presentation/base/base_view_model.dart
@@ -0,0 +1,11 @@
+abstract class BaseViewModel{
+
+}
+abstract class BaseViewModelInputs{
+  void start();  // when view model start
+
+  void dispose(); // when view model gonna die
+}
+abstract class BaseViewModelOutPuts{
+
+}
