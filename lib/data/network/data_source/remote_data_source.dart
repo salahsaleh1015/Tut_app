fbb14af17dd3fb032f5fe34cc6a2cdd2846ed9ea
salah saleh <salahsaleh1015@gmail.com>
2023-10-08 17:49:43 +0300

    adding remote data source and handle login req

---
+++ lib/data/network/data_source/remote_data_source.dart
@@ -0,0 +1,19 @@
+import 'package:tut_app/data/network/requests.dart';
+import 'package:tut_app/data/responses/responses.dart';
+
+import '../app_api.dart';
+
+abstract class RemoteDataSource {
+  Future<AuthenticationResponse> login(LoginRequest loginRequest);
+}
+
+class RemoteDataSourceImpl implements RemoteDataSource {
+  final AppServiceClient _appServiceClient;
+
+  RemoteDataSourceImpl(this._appServiceClient);
+  @override
+  Future<AuthenticationResponse> login(LoginRequest loginRequest) async {
+    return await _appServiceClient.login(
+        loginRequest.email, loginRequest.password);
+  }
+}
