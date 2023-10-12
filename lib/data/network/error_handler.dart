4d6862d3630cb84e369a0679fc60c83fc7b9a0e6
salah saleh <salahsaleh1015@gmail.com>
2023-10-12 17:14:52 +0300

    add Response message class

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -33,4 +33,23 @@ class ResponseCode{
   static const int unKnown = -7;
 
 }
+class ResponseMassage{
+  static const String success = 'success'; // success with data
+  static const String noContent = 'success'; // success without data {NO CONTENT}
+  static const String badRequest = 'Bad Request, Try Again Later'; // failure, API rejected request
+  static const String unAuthorized = 'Un Authorized user ,Try Again Later'; // failure, user is not authorized
+  static const String forbidden = 'Forbidden Request ,Try Again Later '; // failure, API rejected request
+  static const String internalServerError = 'something went wrong , Try Again Later'; // failure, crash in server side
+
+
+  // local status code
+  static const String connectTimeOut = 'Time Out Error , Try Again Later  ';
+  static const String cancel = 'The Request Canceled, Try Again Later ';
+  static const String receiveTimeOut = 'Time Out Error , Try Again Later  ';
+  static const String sentTimeout ='Time Out Error , Try Again Later  ';
+  static const String cacheError ='Cache Error,Try Again Later ' ;
+  static const String noInternetConnection ='Please Check Your Internet Connection' ;
+  static const String unKnown = 'something went wrong , Try Again Later';
+
+}
 
