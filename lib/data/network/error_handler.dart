a43053f53bee924e4affe10e4c948ab77abce1bd
salah saleh <salahsaleh1015@gmail.com>
2023-10-12 15:35:23 +0300

    add data source enum

---
+++ lib/data/network/error_handler.dart
@@ -0,0 +1,15 @@
+enum DataSource {
+  success,
+  noContent,
+  badRequest,
+  forbidden,
+  unAuthorized,
+  noFound,
+  internalServerError,
+  connectTimeOut,
+  cancel,
+  receiveTimeOut,
+  sentTimeout,
+  cacheError,
+  noInternetConnection
+}
