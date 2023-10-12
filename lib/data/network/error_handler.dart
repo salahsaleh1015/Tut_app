89d6080be49eb4fc4f02101e53639c0be5fbb710
salah saleh <salahsaleh1015@gmail.com>
2023-10-12 16:10:42 +0300

    add Response Code class

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -4,12 +4,33 @@ enum DataSource {
   badRequest,
   forbidden,
   unAuthorized,
-  noFound,
   internalServerError,
   connectTimeOut,
   cancel,
   receiveTimeOut,
   sentTimeout,
   cacheError,
-  noInternetConnection
+  noInternetConnection,
+  unKnown
 }
+
+class ResponseCode{
+  static const int success = 200; // success with data
+  static const int noContent = 201; // success without data {NO CONTENT}
+  static const int badRequest = 400; // failure, API rejected request
+  static const int unAuthorized = 401; // failure, user is not authorized
+  static const int forbidden = 403; // failure, API rejected request
+  static const int internalServerError = 500; // failure, crash in server side
+
+
+  // local status code
+  static const int connectTimeOut = -1;
+  static const int cancel = -2;
+  static const int receiveTimeOut = -3;
+  static const int sentTimeout = -4;
+  static const int cacheError = -5;
+  static const int noInternetConnection = -6;
+  static const int unKnown = -7;
+
+}
+
