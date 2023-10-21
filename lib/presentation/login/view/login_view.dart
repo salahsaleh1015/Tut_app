5a5a4a1f75356b060a7f6deabc56eafd5778a57b
salah saleh <salahsaleh1015@gmail.com>
2023-10-21 15:31:32 +0300

    bind view model with view

--- lib/presentation/login/view/login_view.dart
+++ lib/presentation/login/view/login_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tut_app/presentation/login/view_model/login_view_model.dart';
+
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
 
@@ -7,8 +9,36 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final LoginViewModel _viewModel = LoginViewModel(_loginUseCase);
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _userNameController = TextEditingController();
+
+  _bind() {
+    _viewModel.start();
+
+    _passwordController.addListener(() {
+      _viewModel.setPassword(_passwordController.text);
+    });
+
+    _userNameController.addListener(() {
+      _viewModel.setUserName(_userNameController.text);
+    });
+  }
+
+  @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
   }
 }
