8c21775943816833f3a0a4003f72c7b8d8684723
salah saleh <salahsaleh1015@gmail.com>
2023-11-08 09:00:35 +0200

    add state renderer impl

---
+++ lib/presentation/common/state_renderer/state_rendrer_impl.dart
@@ -0,0 +1,58 @@
+import 'package:tut_app/application/costants.dart';
+import 'package:tut_app/presentation/common/state_renderer/state_renderer.dart';
+import 'package:tut_app/presentation/resources/string_manager.dart';
+
+abstract class FlowState {
+  StateRendererType getStateRendererType();
+  String getMessage();
+}
+
+//loading flow (popup - full screen)
+class LoadingState extends FlowState {
+  StateRendererType stateRendererType;
+  String? message;
+
+  LoadingState(
+      {required this.stateRendererType, this.message = StringManager.loading});
+
+  @override
+  String getMessage() => message ?? StringManager.loading;
+
+  @override
+  StateRendererType getStateRendererType() => stateRendererType;
+}
+
+//error flow (popup - full screen)
+class ErrorState extends FlowState {
+  StateRendererType stateRendererType;
+  String message;
+
+  ErrorState(this.stateRendererType, this.message);
+
+  @override
+  String getMessage() => message;
+
+  @override
+  StateRendererType getStateRendererType() => stateRendererType;
+}
+
+// content state
+class ContentState extends FlowState {
+  @override
+  String getMessage() => Constants.empty;
+
+  @override
+  StateRendererType getStateRendererType() => StateRendererType.contentState;
+}
+
+// empty state
+class EmptyState extends FlowState {
+  String message;
+
+  EmptyState(this.message);
+  @override
+  String getMessage() => message;
+
+  @override
+  StateRendererType getStateRendererType() => StateRendererType.contentState;
+}
