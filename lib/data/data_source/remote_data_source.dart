1f9ba48e4539c9b4c38beee583c04466ae04933c
salah saleh <salahsaleh1015@gmail.com>
2023-11-17 16:53:21 +0200

    add reset func in data source

--- lib/data/data_source/remote_data_source.dart
+++ lib/data/data_source/remote_data_source.dart
@@ -2,12 +2,11 @@ import 'package:tut_app/data/network/api/requests.dart';
 import 'package:tut_app/data/responses/login_response/responses.dart';
 
 import '../network/api/app_api.dart';
-
-
+import '../responses/forget_password_response/forget_password_response.dart';
 
 abstract class RemoteDataSource {
   Future<AuthenticationResponse> login(LoginRequest loginRequest);
-
+  Future<ForgetPasswordResponse> reset(ResetRequest resetRequest);
 }
 
 class RemoteDataSourceImpl implements RemoteDataSource {
@@ -18,5 +17,10 @@ class RemoteDataSourceImpl implements RemoteDataSource {
   Future<AuthenticationResponse> login(LoginRequest loginRequest) async {
     return await _appServiceClient.login(
         loginRequest.email, loginRequest.password);
+  }
+
+  @override
+  Future<ForgetPasswordResponse> reset(ResetRequest resetRequest) async {
+    return await _appServiceClient.reset(resetRequest.email);
   }
 }
