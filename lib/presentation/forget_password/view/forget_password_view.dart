42f0bb4f19041ad2a0471b49932e380755d35ce6
salah saleh <salahsaleh1015@gmail.com>
2023-11-18 16:17:38 +0200

    bind view with view model

--- lib/presentation/forget_password/view/forget_password_view.dart
+++ lib/presentation/forget_password/view/forget_password_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tut_app/presentation/forget_password/view_model/forget_password_view_model.dart';
 
 class ForgetPasswordView extends StatefulWidget {
   const ForgetPasswordView({super.key});
@@ -8,8 +9,28 @@ class ForgetPasswordView extends StatefulWidget {
 }
 
 class _ForgetPasswordViewState extends State<ForgetPasswordView> {
+  final ForgetPasswordViewModel _viewModel = ForgetPasswordViewModel(_forgetPasswordUseCase);
+  final TextEditingController _userNameController = TextEditingController();
+
+
+  _bind(){
+    _userNameController.addListener(() {
+      _viewModel.setUserName(_userNameController.text);
+    });
+  }
+  @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+
+  @override
+  void dispose() {
+   _viewModel.dispose();
+    super.dispose();
   }
 }
