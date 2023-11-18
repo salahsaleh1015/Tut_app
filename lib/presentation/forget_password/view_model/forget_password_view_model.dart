a19c82f69f17e2b4b96bf0fe6fd830ee8447d9e4
salah saleh <salahsaleh1015@gmail.com>
2023-11-18 16:13:08 +0200

    add login logic

--- lib/presentation/forget_password/view_model/forget_password_view_model.dart
+++ lib/presentation/forget_password/view_model/forget_password_view_model.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:tut_app/domain/usecase/forget_password_usecase.dart';
 import 'package:tut_app/presentation/base/base_view_model.dart';
+import 'package:tut_app/presentation/common/freezed_data_classes.dart';
 import 'package:tut_app/presentation/common/state_renderer/state_rendrer_impl.dart';
 
 class ForgetPasswordViewModel
@@ -10,6 +12,11 @@ class ForgetPasswordViewModel
         ForgetPasswordViewModelOutput {
   final StreamController _userNameStreamController =
       StreamController<String>.broadcast();
+
+  var forgetPasswordObject = ForgetPasswordObject("");
+
+  final ForgetPasswordUseCase _forgetPasswordUseCase;
+  ForgetPasswordViewModel(this._forgetPasswordUseCase);
 
   @override
   void start() {
@@ -26,31 +33,28 @@ class ForgetPasswordViewModel
   Sink get inputUserName => _userNameStreamController.sink;
 
   @override
-  Stream<bool> get outputUserName =>
-      _userNameStreamController.stream.map((userName) =>_isUserNameValid (userName));
+  Stream<bool> get outputUserName => _userNameStreamController.stream
+      .map((userName) => _isUserNameValid(userName));
   bool _isUserNameValid(String userName) {
     return userName.isNotEmpty;
   }
 
-
   @override
-  reset() {
-    // TODO: implement reset
-    throw UnimplementedError();
+  reset() async {
+    (await _forgetPasswordUseCase
+            .execute(ResetUseCaseInput(forgetPasswordObject.userName)))
+        .fold((failure) => null, (data) => null);
   }
 
   @override
   setUserName(String userName) {
-    // TODO: implement setUserName
-    throw UnimplementedError();
+    inputUserName.add(userName);
+    forgetPasswordObject = forgetPasswordObject.copyWith(userName: userName);
   }
-
-
 
   @override
   // TODO: implement inputState
   Sink get inputState => throw UnimplementedError();
-
 
   @override
   // TODO: implement outputState
