410fd4f6e842bd856d7b089dc0c0c6274479e361
salah saleh <salahsaleh1015@gmail.com>
2023-11-22 15:52:22 +0200

    add input and output register classes

--- lib/presentation/register/view_model/register_view_model.dart
+++ lib/presentation/register/view_model/register_view_model.dart
@@ -1,25 +1,24 @@
-
-
-
 import 'dart:async';
 import 'dart:io';
 import 'package:tut_app/presentation/base/base_view_model.dart';
 
 class RegisterViewModel extends BaseViewModel {
-
-  StreamController userNameStreamController = StreamController<String>.broadcast();
-  StreamController phoneNumberStreamController = StreamController<String>.broadcast();
+  StreamController userNameStreamController =
+      StreamController<String>.broadcast();
+  StreamController phoneNumberStreamController =
+      StreamController<String>.broadcast();
   StreamController emailStreamController = StreamController<String>.broadcast();
-  StreamController passwordStreamController = StreamController<String>.broadcast();
-  StreamController profilePictureStreamController = StreamController<File>.broadcast();
-  StreamController areAllInputsValidStreamController = StreamController<void>.broadcast();
-
+  StreamController passwordStreamController =
+      StreamController<String>.broadcast();
+  StreamController profilePictureStreamController =
+      StreamController<File>.broadcast();
+  StreamController areAllInputsValidStreamController =
+      StreamController<void>.broadcast();
 
   @override
   void start() {
     // TODO: implement start
   }
-
 
   @override
   void dispose() {
@@ -29,7 +28,33 @@ class RegisterViewModel extends BaseViewModel {
     passwordStreamController.close();
     profilePictureStreamController.close();
     areAllInputsValidStreamController.close();
-
   }
+}
 
+abstract class RegisterViewModelInput {
+  Sink get userNameInput;
+  Sink get phoneNumberInput;
+  Sink get emailInput;
+  Sink get passwordInput;
+  Sink get profilePictureInput;
+}
+
+abstract class RegisterViewModelOutput {
+  Stream<bool> get outputIsUserNameValid;
+  Stream<String?> get outputErrorUserName;
+
+
+  Stream<bool> get outputIsPhoneNumberValid;
+  Stream<String?> get outputErrorPhoneNumber;
+
+
+  Stream<bool> get outputIsEmailIValid;
+  Stream<String?> get outputErrorEmail;
+
+
+  Stream<bool> get outputIsPasswordValid;
+  Stream<String?> get outputErrorPassword;
+
+  Stream<bool> get outputIsProfilePictureValid;
+  Stream<String?> get outputErrorProfilePicture;
 }
