04f9c1ff5f595c29105166846647cd4621c2745e
salah saleh <salahsaleh1015@gmail.com>
2023-12-03 14:25:25 +0200

    add stream builder to register view

--- lib/presentation/register/view/register_view.dart
+++ lib/presentation/register/view/register_view.dart
@@ -1,7 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:tut_app/application/di.dart';
 import 'package:tut_app/presentation/register/view_model/register_view_model.dart';
+import 'package:tut_app/presentation/resources/color_maneger.dart';
+import 'package:tut_app/presentation/resources/values_maneger.dart';
+
+import '../../common/state_renderer/state_rendrer_impl.dart';
+
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
 
@@ -10,14 +14,17 @@ class RegisterView extends StatefulWidget {
 }
 
 class _RegisterViewState extends State<RegisterView> {
-
   final RegisterViewModel _viewModel = instance<RegisterViewModel>();
   final _formKey = GlobalKey<FormState>();
-  final TextEditingController _userNameTextEditingController = TextEditingController();
-  final TextEditingController _phoneNumberTextEditingController = TextEditingController();
-  final TextEditingController _emailTextEditingController = TextEditingController();
-  final TextEditingController _passwordTextEditingController = TextEditingController();
-  _bind(){
+  final TextEditingController _userNameTextEditingController =
+      TextEditingController();
+  final TextEditingController _phoneNumberTextEditingController =
+      TextEditingController();
+  final TextEditingController _emailTextEditingController =
+      TextEditingController();
+  final TextEditingController _passwordTextEditingController =
+      TextEditingController();
+  _bind() {
     _userNameTextEditingController.addListener(() {
       _viewModel.setUserName(_userNameTextEditingController.text);
     });
@@ -32,7 +39,6 @@ class _RegisterViewState extends State<RegisterView> {
     _passwordTextEditingController.addListener(() {
       _viewModel.setPassword(_passwordTextEditingController.text);
     });
-
   }
 
   @override
@@ -41,17 +47,36 @@ class _RegisterViewState extends State<RegisterView> {
     _viewModel.start();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: ColorManager.white,
+      appBar: AppBar(
+        elevation: AppSize.s0,
+        backgroundColor: ColorManager.white,
+        iconTheme: IconThemeData(color: ColorManager.black, size: AppSize.s16),
+      ),
+      body: StreamBuilder<FlowState>(
+        stream: _viewModel.outputState,
+        builder: (context, snapshot) {
+          return snapshot.data?.getScreenWidget(context, _getContentWidget(),
+                  () {
+                _viewModel.register();
+              }) ??
+              _getContentWidget();
+        },
+      ),
+    );
   }
 
-
-
+  Widget _getContentWidget() {
+    return Container();
+  }
 
   @override
   void dispose() {
-   _viewModel.dispose();
+    _viewModel.dispose();
     super.dispose();
   }
 }
