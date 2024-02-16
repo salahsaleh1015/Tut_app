419475d65d77b5d1d787081b606778ad2cfab29a
Lenovo <salahsaleh@gmail.com>
2024-02-16 14:29:18 +0200

    edit app service client with home details g file

--- lib/data/network/api/app_api.g.dart
+++ lib/data/network/api/app_api.g.dart
@@ -148,6 +148,33 @@ class _AppServiceClient implements AppServiceClient {
     return value;
   }
 
+  @override
+  Future<HomeDetailsResponse> getHomeDetailsData() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<HomeDetailsResponse>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/detailsScreen/1',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = HomeDetailsResponse.fromJson(_result.data!);
+    return value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
